8dfd5ad631c8706e0022fa59f75a235c2dc0a185
Horrow <faik2k@gmail.com>
2023-07-06 02:50:08 +1000

    test.py shanged

--- test.py
+++ test.py
@@ -2,23 +2,3 @@ phone_book = [{'id':1, 'name':'Емельянов Богдан', 'phone':'891415
               {'id':2, 'name':'Панфилов Кирилл', 'phone':'8909***2021', 'comment':'Семинарист GB'},
               {'id':3, 'name':'Тест', 'phone':'000', 'comment':'Тестовый контакт'}]
 
-
-def prepare_to_save_file(phone_book: list[dict]):   # Преобразование листа справочника к формату для записи в текстовый файл
-    new = []
-    count = 0
-    for item in phone_book:
-        new += item['id'], ':', item['name'], ':', item['phone'], ':', item['comment']
-        if count < len(phone_book) - 1:
-            new += ' /n|'
-        count += 1
-    i = 0
-    while i < len(new):
-        new[i] = str(new[i])
-        i += 1
-    new = [''.join(new)]
-    return new
-
-
-print(prepare_to_save_file(phone_book))
-
-
